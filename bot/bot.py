8206f0c4de7d1dee390a6e5f3bda60ad1d06b1fe
AndreyGolovin85 <47321231+AndreyGolovin85@users.noreply.github.com>
2024-11-04 23:37:58 +0300

    Минимальный рабочий код бота

---
+++ bot/bot.py
@@ -0,0 +1,88 @@
+import asyncio
+
+from aiogram import Bot, types, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.filters.command import Command, CommandObject
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.utils.deep_linking import create_start_link
+import settings as setting
+
+bot = Bot(token=setting.API_TOKEN)
+ADMIN_ID = int(setting.ADMIN_ID)
+dispatcher = Dispatcher()
+
+
+class TimeTracking(StatesGroup):
+    start_time = State()
+    end_time = State()
+
+
+async def generate_start_link(our_bot: Bot):
+    return await create_start_link(our_bot, setting.ACCESS_KEY)
+
+
+@dispatcher.message(Command("help"))
+async def cmd_help(message: types.Message):
+    await message.answer(
+        "Основные команды для работы:\n"
+        "/start_work - команда для регистрации пользователя.\n",
+        parse_mode=ParseMode.HTML,
+    )
+
+
+@dispatcher.message(Command("start"))
+async def cmd_start(message: types.Message, command: CommandObject):
+    if command.args == setting.ACCESS_KEY:
+        await message.answer(
+            "Привет! Я бот для записи и подсчета отработанных часов.\n\n"
+            "Чтобы начать запись, отправьте команду /start_work.\n"
+            "Или воспользуйтесь помощью по командам /help."
+        )
+
+
+@dispatcher.message(Command("start_work"))
+async def cmd_start_work(message: types.Message, state: FSMContext) -> None:
+    await message.reply("Отправьте время начала работы в формате ЧЧ:ММ.")
+    await state.set_state(TimeTracking.start_time)
+
+
+@dispatcher.message(TimeTracking.start_time)
+async def process_name_and_department(message: types.Message, state: FSMContext) -> None:
+    start_time = message.text
+    await message.reply(f"Время начала работы {start_time}.")
+    await state.update_data(start_time=start_time)
+    await message.reply("Отправьте время окончания работы в формате ЧЧ:ММ.")
+    await state.set_state(TimeTracking.end_time)
+
+
+@dispatcher.message(TimeTracking.end_time)
+async def process_department(message: types.Message, state: FSMContext) -> None:
+    end_time = message.text
+    if end_time is None:
+        await message.reply("Неверный формат. Введите время.")
+        return
+    await state.update_data(end_time=end_time)
+    data = await state.get_data()
+
+    await message.reply(
+        "Проверьте данные и подтвердите.\n"
+        f"Время начала работы: {data.get('start_time')}\n"
+        f"Время окончания работы: {data.get('end_time')}\n"
+    )
+
+
+async def main():
+    await bot.send_message(
+        chat_id=ADMIN_ID,
+        text=f"Бот запущен, приглашение работает по ссылке {await generate_start_link(bot)}",
+    )
+    await dispatcher.start_polling(bot, skip_updates=True)
+
+
+if __name__ == "__main__":
+    setting.setup_logging(log_file="bot.log")
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Остановка сервера!")
---
+++ bot/settings.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+import sys
+import logging
+
+
+load_dotenv()
+API_TOKEN = os.getenv("API_TOKEN")
+ADMIN_ID = os.getenv("ADMIN_ID")
+ACCESS_KEY = os.getenv("ACCESS_KEY")
+
+#engine = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+engine = "sqlite:///bot.db"
+
+
+def setup_logging(log_file, level=logging.INFO):
+    """
+    Настройка базового логгера.
+    """
+
+    logging.basicConfig(
+        level=level,
+        format="[%(asctime)s] - %(filename)s:%(lineno)d #%(levelname)-s - %(name)s - %(message)s",
+        filename=log_file,
+        filemode="w",
+    )
+
+    if not API_TOKEN or not ADMIN_ID or not ACCESS_KEY:
+        logging.error("Отсутствуют переменные ENV.")
+        sys.exit(1)
+
+
+now = datetime.now()
+year = now.strftime("%Y")
+
+month_dict = {"yanvar": "Январь", "fevral": "Февраль", "mart": "Март", "aprel": "Апрель", "maj": "Май", "iyun": "Июнь",
+              "iyul": "Июль", "avgust": "Август", "sentyabr": "Сентябрь", "oktbyar": "Октябрь", "noyabr": "Ноябрь",
+              "dekabr": "Декабрь"}
