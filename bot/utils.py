79d0d3865b6eb5d89777d919feb9ab11bf7ea02d
AndreyGolovin85 <47321231+AndreyGolovin85@users.noreply.github.com>
2024-11-17 22:35:37 +0300

    Исправления подсчета времени

--- bot/utils.py
+++ bot/utils.py
@@ -23,8 +23,12 @@ def count_work_time(start_time: str, end_time: str) -> float:
         start_hours, start_minutes = map(int, start_time.split(':'))
         end_hours, end_minutes = map(int, end_time.split(':'))
         total_minutes = (end_hours * 60 + end_minutes) - (start_hours * 60 + start_minutes)
-        total_hours = total_minutes // 60
-        total_minutes %= 60
+        if start_hours < end_hours:
+            total_hours = total_minutes // 60
+            total_minutes %= 60
+        else:
+            total_hours = 24 + (total_minutes // 60)
+            total_minutes %= 60
         if total_hours <= 4:
             total_hours = total_hours
         else:
